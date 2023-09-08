a9adb7e8dacd5a6d1a0132ecb9185868e9e7c87a
Bahaa Youssof <bahaayoussof@gmail.com>
2023-09-08 14:58:33 +0300

    add ProductSwiper

---
+++ src/components/ProductSwiper.jsx
@@ -0,0 +1,37 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { PRODUCTS } from "./../utils/services/ProductsData";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import classes from "./product-swiper.module.scss";
+import ProductCard from "./ProductCard";
+import { Navigation, Pagination } from "swiper";
+
+const ProductSwiper = props => {
+	const { productId } = props;
+
+	const receivedProduct = id => {
+		productId(id);
+	};
+
+	return (
+		<div className={classes.container}>
+			<Swiper
+				spaceBetween={0}
+				slidesPerView={4}
+				pagination={{
+					type: "progressbar",
+				}}
+				modules={[Pagination, Navigation]}
+			>
+				{PRODUCTS?.map(product => (
+					<SwiperSlide key={product.id}>
+						<ProductCard product={product} receivedProduct={receivedProduct} />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
+
+export default ProductSwiper;
