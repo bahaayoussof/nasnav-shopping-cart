26d9447f4b9d61c8008e17a2c440bad0dd87b020
Bahaa Youssof <bahaayoussof@gmail.com>
2023-09-08 08:50:51 +0300

    configure routes, add Home & Product pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { lazy, Suspense } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+
+const ProductPage = lazy(() => import("./pages/Product"));
+
 function App() {
-	return (
-		<>
-			<div>Hello ABDUO MOSTAFA !</div>
-		</>
-	);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout />,
+			children: [
+				{ index: true, element: <Home /> },
+				{
+					path: "product",
+					element: (
+						<Suspense fallback={<div>Loading...</div>}>
+							<ProductPage />
+						</Suspense>
+					),
+				},
+			],
+		},
+	]);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import classes from "./product.module.scss";
+class Product extends Component {
+	render() {
+		return <div>Product</div>;
+	}
+}
+
+export default Product;
